5742d75c974e1efd30c22b0cd43b472c1b60ab9c
yani-valeva <yani.valeva@gmail.com>
2019-06-13 11:46:57 +0300

    Group IssueList and RepoList in pages

--- src/App/App.js
+++ src/App/App.js
@@ -3,9 +3,9 @@ import Header from '../components/Header/Header';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faSearch, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import RepoList from '../RepoList/RepoList';
+import RepoList from '../pages/RepoList/RepoList';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import IssueList from '../IssueList/IssueList';
+import IssueList from '../pages/IssueList/IssueList';
 import Message from '../components/Message/Message';
 
 library.add(fab, faSearch, faChevronRight, faChevronLeft);
